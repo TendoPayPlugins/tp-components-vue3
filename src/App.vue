48256996eff9e6c468d4c9a884ae73fadcb9733f
Igi Espiritu <igi@tendopay.ph>
2024-04-24 21:59:47 +0800

    remove component on the app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <script setup lang="ts">
-import { SideMenu } from "~/components";
-
-function clickout(e) {
-  console.log('clickout', e)
-}
 </script>
 
 <template>
-  <SideMenu
-    @clickout="clickout"
-    :menu="{0:{name:'Verifications',icon:'EyeIcon',current:true,expanded:true,children:{0:{name:'Status',href:'#',current:false},1:{name:'Users',href:'#',current:false},2:{name:'Repayments Due',href:'#',current:false},3:{name:'Quiz Questions',href:'#',current:false},4:{name:'Record Type',href:'#',current:false},5:{name:'Lazada Email',href:'#',current:false},6:{name:'Upload Credit Limit',href:'#',current:false},7:{name:'Upload BIR',href:'#',current:false},8:{name:'Upload Tags',href:'#',current:false}}},1:{name:'Collections',icon:'QueueListIcon',href:'#',current:false,expanded:false,children:{0:{name:'Status',href:'#',current:false},1:{name:'Users',href:'#',current:false},2:{name:'Repayments Due',href:'#',current:false},3:{name:'Quiz Questions',href:'#',current:false},4:{name:'Record Type',href:'#',current:false},5:{name:'Lazada Email',href:'#',current:false},6:{name:'Upload Credit Limit',href:'#',current:false},7:{name:'Upload BIR',href:'#',current:false},8:{name:'Upload Tags',href:'#',current:false}}},2:{name:'3rd Party',icon:'QueueListIcon',href:'#',current:false,expanded:false,children:{0:{name:'Status',href:'#',current:false},1:{name:'Users',href:'#',current:false},2:{name:'Repayments Due',href:'#',current:false},3:{name:'Quiz Questions',href:'#',current:false},4:{name:'Record Type',href:'#',current:false},5:{name:'Lazada Email',href:'#',current:false},6:{name:'Upload Credit Limit',href:'#',current:false},7:{name:'Upload BIR',href:'#',current:false},8:{name:'Upload Tags',href:'#',current:false}}},3:{name:'Event',icon:'QueueListIcon',href:'#',current:false,expanded:false,children:{0:{name:'Status',href:'#',current:false},1:{name:'Users',href:'#',current:false},2:{name:'Repayments Due',href:'#',current:false},3:{name:'Quiz Questions',href:'#',current:false},4:{name:'Record Type',href:'#',current:false},5:{name:'Lazada Email',href:'#',current:false},6:{name:'Upload Credit Limit',href:'#',current:false},7:{name:'Upload BIR',href:'#',current:false},8:{name:'Upload Tags',href:'#',current:false}}}}"
-  />
+  <div>
+    TpComponents
+  </div>
 </template>
