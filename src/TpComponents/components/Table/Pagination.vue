c140fce2a8e3a3f2378e255b523d69457f7cd082
Igi Espiritu <igi@tendopay.ph>
2024-04-24 22:27:04 +0800

    set default primary color on all components

--- src/TpComponents/components/Table/Pagination.vue
+++ src/TpComponents/components/Table/Pagination.vue
@@ -28,7 +28,7 @@
                         <span class="sr-only">Previous</span>
                         <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
                     </a>
-                    <a href="#" aria-current="page" class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">1</a>
+                    <a href="#" aria-current="page" class="relative z-10 inline-flex items-center bg-tp-primary px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-tp-primary">1</a>
                     <a href="#" class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">2</a>
                     <a href="#" class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">3</a>
                     <span class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">...</span>
--- src/TpComponents/components/Table/Table.vue
+++ src/TpComponents/components/Table/Table.vue
@@ -22,7 +22,7 @@
                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.email }}</td>
                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.role }}</td>
                         <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                            <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                            <a href="#" class="text-tp-primary hover:text-tp-primary-dark"
                             >Edit<span class="sr-only">, {{ person.name }}</span></a
                             >
                         </td>
