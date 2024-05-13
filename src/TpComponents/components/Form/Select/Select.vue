7b26d6fbcf3a72a184655ff761b2327316c9e88b
Szymon Sterczewski <szymon.sterczewski@gmail.com>
2024-05-13 15:48:44 +0200

    Fix onInput

--- src/TpComponents/components/Form/Select/Select.vue
+++ src/TpComponents/components/Form/Select/Select.vue
@@ -67,7 +67,6 @@ watch(localValue, onInput);
       :disabled="disabled"
       v-model="localValue"
       :data-test="dataTest + '-select'"
-      @change="onInput"
       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-tp-primary block w-full p-2.0 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     >
       <option v-if="placeholder" :value="[]" disabled hidden>
