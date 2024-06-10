91946f44ea2fbd224c9bf6fb0e28cede39e1b172
Szymon Sterczewski <szymon.sterczewski@gmail.com>
2024-06-10 13:14:49 +0200

    Remove scroll from input number

--- src/TpComponents/components/Form/Inputs/InputNumber.vue
+++ src/TpComponents/components/Form/Inputs/InputNumber.vue
@@ -118,3 +118,17 @@ watch(localValue, onInput);
     </span>
   </div>
 </template>
+
+<style>
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+    -moz-appearance: textfield;
+}
+</style>
