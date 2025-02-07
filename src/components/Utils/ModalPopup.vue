8b28853160afe1021023145212762709ba03276f
Igi Espiritu <igi@tendopay.ph>
2025-02-07 17:37:14 +0800

    add props zindex and size for the modal popup component

--- src/components/Utils/ModalPopup.vue
+++ src/components/Utils/ModalPopup.vue
@@ -6,7 +6,7 @@
   >
     <Dialog
       as="div"
-      :class="`relative z-${zindex}`"
+      :class="`relative ${modalIndex[zindex]}`"
     >
       <TransitionChild
         as="template"
@@ -19,11 +19,11 @@
       >
         <div
           v-if="props.backdrop"
-          class="fixed tc-inset-0 tc-bg-gray-500 tc-bg-opacity-75 tc-transition-opacity"
+          :class="`fixed ${modalIndex[zindex]} tc-inset-0 tc-bg-gray-500 tc-bg-opacity-75 tc-transition-opacity`"
         />
       </TransitionChild>
 
-      <div class="tc-fixed tc-inset-0 tc-z-10 tc-w-full tc-overflow-y-auto">
+      <div :class="`tc-fixed tc-inset-0 ${modalIndex[zindex]} tc-w-full tc-overflow-y-auto`">
         <div class="tc-flex tc-min-h-full tc-items-end tc-justify-center tc-p-4 tc-text-center sm:tc-items-center sm:tc-p-0">
           <TransitionChild
             as="template"
@@ -35,7 +35,7 @@
             leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <DialogPanel
-              class="tc-relative tc-transform tc-overflow-hidden tc-rounded-lg tc-bg-white tc-px-4 tc-pb-4 tc-pt-5 tc-text-left tc-shadow-xl tc-transition-all lg:tc-w-3/4 sm:tc-my-8 sm:tc-w-full sm:tc-max-w-lg sm:tc-p-6"
+              :class="`tc-relative tc-transform tc-overflow-hidden tc-rounded-lg tc-bg-white tc-px-4 tc-pb-4 tc-pt-5 tc-text-left tc-shadow-xl tc-transition-all sm:tc-my-8 sm:tc-max-w-lg sm:tc-p-6 ${modalSize[size]}`"
             >
               <div class="tc-absolute tc-right-0 tc-top-0 tc-pr-4 tc-pt-4 sm:tc-block">
                 <button
@@ -125,13 +125,26 @@ const props = defineProps({
   },
   size: {
     type: String,
-    default: 'md',
+    default: 'sm',
   },
   zindex: {
     type: Number,
     default: 10
   }
 })
+
+const modalSize = {
+  sm: "tc-min-w-lg",
+  md: "tc-min-w-2xl",
+  lg: "tc-min-w-5xl",
+}
+
+const modalIndex = {
+  10: "tc-z-10",
+  15: "tc-z-15",
+  20: "tc-z-20",
+  25: "tc-z-25",
+}
 
 function doCloseOutside() {
   if (props.closeOutside) {
