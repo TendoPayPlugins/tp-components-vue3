9c0ed3da2420804083c68fb3706eb5b4e29af059
Igi Espiritu <igi@tendopay.ph>
2025-02-10 22:14:04 +0800

    update DownloadButton

--- src/components/Form/Buttons/DownloadButton.vue
+++ src/components/Form/Buttons/DownloadButton.vue
@@ -21,9 +21,9 @@ const props = defineProps({
     type: String,
     required: true,
   },
-  drop: {
-    type: Boolean,
-    default: false
+  options: {
+    type: Array,
+    default: () => []
   }
 });
 
@@ -53,7 +53,7 @@ watch(() => props.data, async (newVal, oldVal) => {
 </script>
 
 <template>
-  <template v-if="props.drop">
+  <template v-if="props.options.length >= 1">
     <Menu as="div" class="relative inline-block text-left">
       <div>
         <MenuButton
@@ -62,35 +62,19 @@ watch(() => props.data, async (newVal, oldVal) => {
           class="tc-flex tc-items-center tc-bg-transparent tc-border-2 tc-border-tp-grey-600 tc-rounded-md tc-px-4 tc-py-2"
         >
           <ArrowDownIcon class="tc-size-3 tc-mr-3" />
-          Download
+          <span class="tc-font-bold tc-text-sm">Download</span>
         </MenuButton>
       </div>
 
       <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-        <MenuItems class="tc-absolute tc-right-0 tc-z-10 tc-mt-2 tc-w-56 tc-origin-top-right tc-rounded-md tc-bg-white tc-shadow-lg tc-border tc-border-tp-grey-300 focus:tc-outline-none">
+        <MenuItems class="tc-absolute tc-right-0 tc-z-10 tc-w-56 tc-origin-top-right tc-rounded-md tc-bg-white tc-shadow-lg tc-border tc-border-tp-grey-300 focus:tc-outline-none">
           <div class="py-1">
-            <MenuItem v-slot="{ active }">
+            <MenuItem v-slot="{ active }" v-for="item in props.options">
               <div
                 :class="[active ? 'tc-bg-gray-100 tc-text-gray-900 tc-outline-none' : 'tc-text-gray-700', 'tc-block tc-px-4 tc-py-2 tc-text-sm tc-cursor-pointer']"
-                @click="emit('event1')"
+                @click="item.event"
               >
-                Simplified
-              </div>
-            </MenuItem>
-            <MenuItem v-slot="{ active }">
-              <div
-                :class="[active ? 'tc-bg-gray-100 tc-text-gray-900 tc-outline-none' : 'tc-text-gray-700', 'tc-block tc-px-4 tc-py-2 tc-text-sm tc-cursor-pointer']"
-                @click="emit('event2')"
-              >
-                Detail
-              </div>
-            </MenuItem>
-            <MenuItem v-slot="{ active }">
-              <div
-                :class="[active ? 'tc-bg-gray-100 tc-text-gray-900 tc-outline-none' : 'tc-text-gray-700', 'tc-block tc-px-4 tc-py-2 tc-text-sm tc-cursor-pointer']"
-                @click="emit('event3')"
-              >
-                Block Reason
+                {{ item.label }}
               </div>
             </MenuItem>
           </div>
