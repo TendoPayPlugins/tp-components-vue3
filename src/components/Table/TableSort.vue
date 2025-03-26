7148c34d67d57f44fa514f2a6296567d8062e0c2
Szymon Sterczewski <szymon.sterczewski@gmail.com>
2025-03-26 15:39:41 +0100

    v0.0.33

--- src/components/Table/TableSort.vue
+++ src/components/Table/TableSort.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {ChevronDownIcon, ChevronUpIcon} from "@heroicons/vue/24/solid";
+import {computed} from "vue";
 
 const props = defineProps({
   sort: {
@@ -17,6 +18,10 @@ const props = defineProps({
   }
 })
 
+const isActive = computed(() => {
+  return props.active
+})
+
 const emit = defineEmits(['sort'])
 const updateSort = () => {
   emit('sort', {sort: props.sort, dir: props.dir === 'asc' ? 'desc' : 'asc'})
@@ -29,8 +34,8 @@ const updateSort = () => {
   >
     <slot />
     <span
-        class="tc-ml-2 tc-flex-none tc-rounded group-hover:tc-bg-gray-200"
-        :class="{ 'tc-bg-tonik-purple tc-text-white': props.active, 'tc-bg-gray-100 tc-text-gray-900': !props.active }
+        class="tc-ml-2 tc-flex-none tc-rounded"
+        :class="{ 'tc-bg-tonik-purple tc-text-white': isActive, 'tc-bg-gray-100 tc-text-gray-900': isActive }
     ">
       <ChevronUpIcon
         v-if="props.dir === 'asc'"
