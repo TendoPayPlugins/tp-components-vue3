eb15d35546194bdb624de1df5dad3eda8d853760
Szymon Sterczewski <szymon.sterczewski@gmail.com>
2025-08-28 00:56:01 +0200

    v0.0.73

--- src/components/Form/Date/DatePicker.vue
+++ src/components/Form/Date/DatePicker.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref, computed } from "vue";
+import {ref, watch} from "vue";
 import VueTailwindDatepicker from "./DatePickerComponent/VueTailwindDatePicker.vue";
+const emit = defineEmits(['input', 'update:modelValue'])
 
-const modelArray = defineModel({type: Array, default: []})
+const localValue = defineModel({type: Array, default: []})
 
 const formatter = ref({
   date: 'YYYY-MM-DD',
@@ -11,45 +12,59 @@ const formatter = ref({
 })
 
 const props = defineProps({
-  disabled: {type: Boolean, default: false},
-  dataTest: {type: String, required: true},
-  placeholder: {type: String, default: 'Select'},
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  dataTest: {
+    type: String,
+    required: true
+  },
+  placeholder: {
+    type: String,
+    default: 'Select'
+  },
   v: {
-    type: Object, default: () => {
+    type: Object,
+    default: () => {
     }
   },
-  inline: {type: Boolean, default: false},
-  showError: {type: Boolean, default: true},
-})
-
-const singleProxy = computed({
-  get() {
-    return modelArray.value?.[0] ?? null
+  inline: {
+    type: Boolean,
+    default: false,
   },
-  set(val) {
-    modelArray.value = val ? [val] : []
+  showError: {
+    type: Boolean,
+    default: true
   }
 })
+
+const onInput = () => {
+  // noinspection JSCheckFunctionSignatures
+  emit("update:modelValue", localValue.value);
+};
+
+watch(localValue, onInput);
 </script>
 
 <template>
   <vue-tailwind-datepicker
-      v-model="singleProxy"
-      :disabled="disabled"
-      :formatter="formatter"
-      :no-input="inline"
-      :placeholder="placeholder"
-      as-single
+    v-model="localValue"
+    :disabled="disabled"
+    :formatter="formatter"
+    :no-input="inline"
+    :placeholder="placeholder"
+    as-single
   />
   <span v-if="showError && v?.$invalid">
     <p
-        v-for="(error, index) in v?.$silentErrors"
-        :key="index"
-        class="tc-mt-2 tc-text-sm tc-text-red-600 dark:tc-text-red-400"
+      v-for="(error, index) in v?.$silentErrors"
+      :key="index"
+      class="tc-mt-2 tc-text-sm tc-text-red-600 dark:tc-text-red-400"
     >
       <span
-          :data-test="dataTest + '-email-error' + error.$uid"
-          class="tc-font-medium"
+        :data-test="dataTest + '-email-error' + error.$uid"
+        class="tc-font-medium"
       >{{ error.$message }}</span>
     </p>
   </span>
