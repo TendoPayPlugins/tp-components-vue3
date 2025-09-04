fb3d0abcb21e4c5e86c0a3e407b8db60f37685ea
Szymon Sterczewski <szymon.sterczewski@gmail.com>
2025-09-04 18:02:06 +0200

    v0.0.83

--- src/components/Utils/ConfirmDialog.vue
+++ src/components/Utils/ConfirmDialog.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { useConfirmDialog } from "../../composables/useConfirmDialog.js"
+import { useConfirmDialog } from "../../composables/index.js"
 import {ref} from "vue";
 import InputText from "../Form/Inputs/InputText.vue";
 import FormButton from "../Form/Buttons/FormButton.vue";
@@ -18,41 +18,49 @@ const onConfirm = () => {
 </script>
 
 <template>
-  <div
-    v-if="isVisible"
-    class="tc-fixed tc-inset-0 tc-flex tc-items-center tc-justify-center tc-bg-black tc-bg-opacity-50 tc-z-50"
-  >
-    <div class="tc-bg-white tc-p-8 tc-rounded-lg tc-shadow-md lg:tc-w-1/3 md:tc-w-1/2 sm:tc-w-auto">
-      <p class="tc-text-lg tc-font-semibold tc-mb-4">
-        {{ title || '-' }}
-      </p>
-      <p class="tc-text-gray-700 tc-mb-6">
-        {{ message || '-' }}
-      </p>
-      <p v-if="password">
-        <InputText
-          v-model="localPassword"
-          data-test="confirm-password"
-        />
-      </p>
-      <div class="tc-flex tc-justify-end">
-        <FormButton
-          type="gray"
-          class="tc-px-4 tc-py-2 tc-mr-2"
-          data-test="cancel-delete"
-          @click="onCancel"
-        >
-          Cancel
-        </FormButton>
-        <FormButton
-          type="danger"
-          data-test="confirm-delete"
-          :disabled="password !== localPassword"
-          @click="onConfirm"
-        >
-          Confirm
-        </FormButton>
+  <Teleport to="body">
+    <div
+        v-if="isVisible"
+        class="tc-fixed tc-inset-0 tc-flex tc-items-center tc-justify-center
+             tc-bg-black tc-bg-opacity-50 tc-z-[9999]"
+    >
+      <div
+          class="tc-bg-white tc-p-8 tc-rounded-lg tc-shadow-md
+               lg:tc-w-1/3 md:tc-w-1/2 sm:tc-w-auto"
+      >
+        <p class="tc-text-lg tc-font-semibold tc-mb-4">
+          {{ title || '-' }}
+        </p>
+        <p class="tc-text-gray-700 tc-mb-6">
+          {{ message || '-' }}
+        </p>
+
+        <p v-if="password">
+          <InputText
+              v-model="localPassword"
+              data-test="confirm-password"
+          />
+        </p>
+
+        <div class="tc-flex tc-justify-end">
+          <FormButton
+              type="gray"
+              class="tc-px-4 tc-py-2 tc-mr-2"
+              data-test="cancel-delete"
+              @click="onCancel"
+          >
+            Cancel
+          </FormButton>
+          <FormButton
+              type="danger"
+              data-test="confirm-delete"
+              :disabled="password !== localPassword"
+              @click="onConfirm"
+          >
+            Confirm
+          </FormButton>
+        </div>
       </div>
     </div>
-  </div>
+  </Teleport>
 </template>
